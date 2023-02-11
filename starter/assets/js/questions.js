392ebf6298aa9e2799a59b173b12432884c4fea2
Ambika Kochuparambil Sidharthan <ak21ace@herts.ac.uk>
2023-02-11 18:54:49 +0000

    Updated with saving score and initial

--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -2,34 +2,36 @@
 var questions=[
     {
         prompt:"1. Which program code doesn’t need preprocessing before being run? ",
-        choices:["a) Text"," b) Script ","c) Both Text and Script ","d) Comment"],
-        answer: "b"
+        options:["Text","Script ","Both Text and Script ","Comment"],
+        answer: "Script "
     },
     {
         prompt:"2. What is the significance of scripting?",
-        choices:[" a) Convenient ","b) Dynamic"," c) Reachable"," d) Modular"],
-        answer:"b"
+        options:["Convenient ","Dynamic","Reachable","Modular"],
+        answer:"Dynamic"
     },
     {
         prompt:"3. Which API makes the user’s current location available to browser-based application?" ,
-        choices:["a) Java API"," b) SDL API ","c) Object API"," d) Geolocation API"],
-        answer:"d"
+        options:["Java API","SDL API ","Object API","Geolocation API"],
+        answer:"Geolocation API"
     },
     {
         prompt:"4. What is the necessity to have API?",
-        Choices:[" a) Guide to performing activities"," b) Describe particular task ","c) Both performing activities and Describe particular task"," d) Rearrangement of tasks"],
-        answer:"b"
+        options:["Guide to performing activities","Describe particular task ","Both performing activities and Describe particular task","Rearrangement of tasks"],
+        answer:"Describe particular task "
     },
     {
-        prompt:"5. 9. When does one use the event ready?",
-        choices:["a) Before loading"," b) During loading","c) After loading"," d) During reloading"],
-        answer:"c"
+        prompt:"5. When does one use the event ready?",
+        options:["Before loading","During loading","After loading","During reloading"],
+        answer:"After loading"
     }
 ]; 
 
+
+//Elements
 var questionsEl = document.querySelector("#questions");
 var timerEl = document.querySelector("#time");
-var choicesEl = document.querySelector("#choices");
+var choicesEl = document.querySelector("#options");
 var submitBtn = document.querySelector("#submit");
 var startBtn = document.querySelector("#start");
 var nameEl = document.querySelector("#initials");
@@ -38,13 +40,12 @@ var reStartBtn = document.querySelector("#restart");
 
 
 //initial state
-
 var currentQuestionIndex = 0;
 var time = questions.length * 15;
 var timerId;
 
-// Start quiz... and hide frontpage
 
+// Start quiz... and hide frontpage
 function quizStart() {
     timerId = setInterval(clockTick, 1000);
     timerEl.textContent = time;
@@ -54,36 +55,40 @@ function quizStart() {
     getQuestion();
 }
 
-// questions and answers  with buttons
 
+// questions and answers 
 function getQuestion() {
     var currentQuestion = questions[currentQuestionIndex];
-  var promptEl = document.getElementById("question-words")
+  var promptEl = document.getElementById("question-title")
     promptEl.textContent = currentQuestion.prompt;
     choicesEl.innerHTML = "";
-    currentQuestion.options.forEach(function(choice, i) {
+    currentQuestion.options.forEach(function(choices, i) {
         var choiceBtn = document.createElement("button");
-        choiceBtn.setAttribute("value", choice);
-        choiceBtn.textContent = i + 1 + ". " + choice;
+        choiceBtn.setAttribute("value", choices);
+        choiceBtn.textContent = i + 1 + ". " + choices;
         choiceBtn.onclick = questionClick;
         choicesEl.appendChild(choiceBtn);
     });
 }
 
-// Check for right answers and reduce time for wrong one , then next question
 
+// Check for right answers and reduce time for wrong one , then next question
 function questionClick() {
+  console.log(this.value);
+  console.log(questions[currentQuestionIndex]);
+  console.log(questions[currentQuestionIndex].answer);
+  console.log(`===========================`);
     if (this.value !== questions[currentQuestionIndex].answer) {
       time -= 10;
       if (time < 0) {
         time = 0;
       }
       timerEl.textContent = time;
-      feedbackEl.textContent = `Wrong! The correct answer was ${questions[currentQuestionIndex].answer}.`;
-      feedbackEl.style.color = "red";
+      feedbackEl.textContent = `👀Wrong! The correct answer was ${questions[currentQuestionIndex].answer}.`;
+      feedbackEl.style.color = "orange";
     } else {
-      feedbackEl.textContent = "Correct!";
-      feedbackEl.style.color = "green";
+      feedbackEl.textContent = "🤩Correct!💃";
+      feedbackEl.style.color = "purple";
     }
     feedbackEl.setAttribute("class", "feedback");
     setTimeout(function() {
@@ -98,19 +103,17 @@ function questionClick() {
 }
 
 // End quiz .. stop timer.. display final score
-
 function quizEnd() {
     clearInterval(timerId);
-    var endScreenEl = document.getElementById("quiz-end");
+    var endScreenEl = document.getElementById("end-screen");
     endScreenEl.removeAttribute("class");
-    var finalScoreEl = document.getElementById("score-final");
+    var finalScoreEl = document.getElementById("final-score");
     finalScoreEl.textContent = time;
     questionsEl.setAttribute("class", "hide");
 }
 
 
-// finish quiz  if timer=0
-
+// finish quiz  when timer is 0
 function clockTick() {
     time--;
     timerEl.textContent = time;
@@ -119,19 +122,24 @@ function clockTick() {
     }
 }
 
-// Save score along with user name
-
+// Save  user name and score
 function saveHighscore() {
     var name = nameEl.value.trim();
     if (name !== "") {
       var highscores =
         JSON.parse(window.localStorage.getItem("highscores")) || [];
+        console.log(`-------777777  ----highscores ${highscores}`);
       var newScore = {
         score: time,
         name: name
       };
+      console.log(`----------------highscores ${highscores}`);
+      console.log(`-------------newscore ${newScore}`);
       highscores.push(newScore);
       window.localStorage.setItem("highscores", JSON.stringify(highscores));
+      
+       console.log(`==---------highscores ${highscores}`);
+      console.log(`=====---------newscore ${newScore}`);
     }
 }
 
