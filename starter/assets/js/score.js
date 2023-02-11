ad3f2eabe9c8e21f8ffafa401952e088c1e8f70b
Ambika Kochuparambil Sidharthan <ak21ace@herts.ac.uk>
2023-02-11 13:39:11 +0000

    Added questions , answers, updated assets

--- starter/assets/js/score.js
+++ starter/assets/js/score.js
@@ -1,12 +1,24 @@
-var score=0;
-for (var i=0;i<questions.length;i++){
-    var response=this.prompt(questions[i].prompt)
-    if (response==questions[i].answer){
-        score++;
-        alert("Correct");
-    }
-    else{
-        alert("WRONG");
-    }
+var scoresBtn = document.querySelector("#view-high-scores");
+
+// Rank previous scores in order by retrieving scores from localStorage
+
+function printHighscores() {
+    var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+    highscores.sort(function(a, b) {
+      return b.score - a.score;
+    });
+    highscores.forEach(function(score) {
+      var liTag = document.createElement("li");
+      liTag.textContent = score.name + " - " + score.score;
+      var olEl = document.getElementById("highscores");
+      olEl.appendChild(liTag);
+    });
 }
- alert(`Your score : ${score}`);
+
+// Clear previous scores when users click clear 
+  function clearHighscores() {
+    window.localStorage.removeItem("highscores");
+    window.location.reload();
+  } document.getElementById("clear").onclick = clearHighscores;
+  
+printHighscores();
