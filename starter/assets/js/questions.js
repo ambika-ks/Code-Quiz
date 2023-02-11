894a671fb4c909fcddae4ce661cf19300ba310be
Ambika Kochuparambil Sidharthan <ak21ace@herts.ac.uk>
2023-02-11 01:49:07 +0000

    Added Elements

--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -26,3 +26,12 @@ var questions=[
     },
 
 ]  ; 
+
+var questionsEl = document.querySelector("#questions");
+var timerEl = document.querySelector("#time");
+var choicesEl = document.querySelector("#choices");
+var submitBtn = document.querySelector("#submit");
+var startBtn = document.querySelector("#start");
+var nameEl = document.querySelector("#initials");
+var feedbackEl = document.querySelector("#feedback");
+var reStartBtn = document.querySelector("#restart");
